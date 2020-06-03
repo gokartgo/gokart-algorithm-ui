0c0f390037543eaf8779a7790e179932c2458efa
gokartgo <gokart.007@hotmail.com>
2020-06-03 15:29:30 +0700

    create start node and finish node

--- src/PathfindingVisualizer/Node/Node.js
+++ src/PathfindingVisualizer/Node/Node.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react'
 import './Node.scss'
 
 class Node extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
 
-  render() {
-    return (
-      <div className='node'></div>
-    )
-  }
+	render() {
+		const { isStart, inFinish } = this.props
+		let className = 'node'
+		if (isStart) {
+			className += ' node-start'
+		}
+		if (inFinish) {
+			className += ' node-finish'
+		}
+		return <div className={className}></div>
+	}
 }
 
 export default Node
+
+export const DEFAULT_NODE = {
+	row: 0,
+	col: 0,
+}
--- src/PathfindingVisualizer/PathfindingVisualizer.js
+++ src/PathfindingVisualizer/PathfindingVisualizer.js
@@ -2,39 +2,53 @@ import React, { PureComponent } from 'react'
 import Node from './Node/Node'
 
 class PathfindingVisualizer extends PureComponent {
-  constructor(props) {
-    super(props)
-    this.state = {
-      nodes: []
-    }
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			nodes: [],
+		}
+	}
 
-  componentDidMount() {
-    const nodes = [];
-    for (let row = 0; row < 15; row++) {
-      const currentRow = [];
-      for (let col = 0; col < 40; col++) {
-        currentRow.push([])
-      }
-      nodes.push(currentRow)
-    }
-    this.setState({ nodes })
-  }
+	componentDidMount() {
+		const nodes = []
+		for (let row = 0; row < 15; row++) {
+			const currentRow = []
+			for (let col = 0; col < 40; col++) {
+				const currnetNode = {
+					row,
+					col,
+					isStart: row === 10 && col === 5 ? true : false,
+					isFinish: row === 10 && col === 35 ? true : false,
+				}
+				currentRow.push(currnetNode)
+			}
+			nodes.push(currentRow)
+		}
+		this.setState({ nodes })
+	}
 
-  render() {
-    const { nodes } = this.state
-    return (
-      <div>
-      {
-        nodes.map((row, rowIndex) => {
-          return <div>
-            {row.map((node, nodeIndex) => <Node></Node>)}
-          </div>
-        })
-      }
-      </div>
-    )
-  }
+	render() {
+		const { nodes } = this.state
+		return (
+			<div>
+				{nodes.map((row, rowIndex) => {
+					return (
+						<div key={`row-${rowIndex}`}>
+							{row.map((node, nodeIndex) => {
+								const { isStart, isFinish } = node
+								return (
+									<Node
+										key={`node-${rowIndex}-${nodeIndex}`}
+										isStart={isStart}
+										inFinish={isFinish}></Node>
+								)
+							})}
+						</div>
+					)
+				})}
+			</div>
+		)
+	}
 }
 
 export default PathfindingVisualizer
