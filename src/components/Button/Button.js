7e780a16db1ec32e190a66e318101063eee4e171
gokartgo <gokart.007@hotmail.com>
2020-06-13 14:43:10 +0700

    finish bubble sort

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Button.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Button.scss'
 
-const button = (props) => (
-    <button
-        disabled={props.disabled}
-        className={`Button ${props.btnType}`}
-        onClick={props.clicked}>{props.children}</button>
-);
+const button = props => (
+	<button
+		disabled={props.disabled}
+		className={`Button ${props.class}`}
+		onClick={props.clicked}>
+		{props.children}
+	</button>
+)
 
-button.propTypes = ({
-  btnType: PropTypes.string
-})
-
-button.defaultProps = {
-  btnType: ''
+button.propTypes = {
+	btnType: PropTypes.string,
 }
 
-export default button;
+button.defaultProps = {
+	btnType: '',
+}
+
+export default button
--- src/containers/SortVisualizer/SortVisualizer.js
+++ src/containers/SortVisualizer/SortVisualizer.js
@@ -1,13 +1,74 @@
 import React, { Component } from 'react'
+import { Fragment, Button } from '/components'
+import BubbleSort from '/algorithms/bubble_sort'
+import './SortVisualizer.scss'
 
 class SortVisualizer extends Component {
 	constructor(props) {
 		super(props)
-		this.state = {}
+		this.state = {
+			array: [],
+			timeouts: [],
+		}
+	}
+
+	componentDidMount() {
+		this.setArray()
+	}
+
+	randomIntFromInterval(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min)
+	}
+
+	setArray() {
+		const array = []
+		for (let i = 0; i < 100; i++) {
+			array.push(this.randomIntFromInterval(5, 400))
+		}
+		this.setState({ array, timeouts: [] })
+	}
+
+	resetArray() {
+		const { timeouts } = this.state
+		for (let i = 0; i < timeouts.length; i++) {
+			clearTimeout(timeouts[i])
+		}
+		for (let i = 0; i < 100; i++) {
+			document.getElementById(`sort-${i}`).classList.remove('bar-select')
+		}
+		this.setArray()
+	}
+
+	bubbleSort() {
+		const { array, timeouts } = this.state
+		const newTimeouts = BubbleSort(array, timeouts)
+		this.setState({ timeouts: newTimeouts })
 	}
 
 	render() {
-		return <div>sort</div>
+		const { array } = this.state
+		return (
+			<Fragment>
+				<div className='bar-container'>
+					{array.map((value, idx) => {
+						return (
+							<div
+								className='bar'
+								style={{ height: `${value}px` }}
+								key={`sort-${idx}`}
+								id={`sort-${idx}`}
+							/>
+						)
+					})}
+				</div>
+				<Button class='' clicked={() => this.resetArray()}>
+					New Pattern
+				</Button>
+				<Button class='' clicked={() => this.bubbleSort()}>
+					Bubble Sort
+				</Button>
+			</Fragment>
+		)
 	}
 }
 
