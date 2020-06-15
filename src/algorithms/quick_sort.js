d691aa16c90a696cce15e86787a39b817eb31b5c
gokartgo <gokart.007@hotmail.com>
2020-06-15 22:05:17 +0700

    set callback for set sort array

--- src/algorithms/quick_sort.js
+++ src/algorithms/quick_sort.js
@@ -1,87 +1,93 @@
+const time = 5
 let delay = 0
 
 const setSelectFrontBar = (index, timeouts) => {
-	timeouts.push(
-		setTimeout(() => {
-			if (index > 0) {
-				document
-					.getElementById(`sort-${index - 1}`)
-					.classList.remove('bar-select')
-			}
-			document.getElementById(`sort-${index}`).classList.add('bar-select')
-		}, delay++ * 2),
-		10,
-	)
+  timeouts.push(
+    setTimeout(() => {
+      if (index > 0) {
+        document
+          .getElementById(`sort-${index - 1}`)
+          .classList.remove('bar-select')
+      }
+      document.getElementById(`sort-${index}`).classList.add('bar-select')
+    }, delay++ * time),
+    10,
+  )
 }
 
 const setSelectBackBar = (index, back, timeouts) => {
-	timeouts.push(
-		setTimeout(() => {
-			if (index < back) {
-				document
-					.getElementById(`sort-${index + 1}`)
-					.classList.remove('bar-select')
-			}
-			document.getElementById(`sort-${index}`).classList.add('bar-select')
-		}, delay++ * 2),
-		10,
-	)
+  timeouts.push(
+    setTimeout(() => {
+      if (index < back) {
+        document
+          .getElementById(`sort-${index + 1}`)
+          .classList.remove('bar-select')
+      }
+      document.getElementById(`sort-${index}`).classList.add('bar-select')
+    }, delay++ * time),
+    10,
+  )
 }
 
 const swap = (array, index_before, index_after, timeouts) => {
-	let temp = array[index_before]
-	array[index_before] = array[index_after]
-	array[index_after] = temp
-	timeouts.push(
-		setTimeout(() => {
-			document.getElementById(
-				`sort-${index_before}`,
-			).style.height = `${array[index_before]}px`
-			document.getElementById(
-				`sort-${index_after}`,
-			).style.height = `${array[index_after]}px`
-		}, delay++ * 2),
-		10,
-	)
+  let temp = array[index_before]
+  array[index_before] = array[index_after]
+  array[index_after] = temp
+  timeouts.push(
+    setTimeout(() => {
+      document.getElementById(
+        `sort-${index_before}`,
+      ).style.height = `${array[index_before]}px`
+      document.getElementById(
+        `sort-${index_after}`,
+      ).style.height = `${array[index_after]}px`
+    }, delay++ * time),
+    10,
+  )
 }
 
 const quicksort = (arr, start, end, timeouts) => {
-	if (start >= end) {
-		return
-	}
-	let i = start,
-		j = end
-	let pivot = arr[start]
-	while (i < j) {
-		while (pivot >= arr[i] && i < j) {
-			setSelectFrontBar(i, timeouts)
-			i++
-		}
-		while (pivot < arr[j] && i <= j) {
-			setSelectBackBar(j, arr.length - 1, timeouts)
-			j--
-		}
-		if (i < j) {
-			swap(arr, i, j, timeouts)
-			j--
-		}
-	}
-	swap(arr, start, j, timeouts)
-	quicksort(arr, start, j - 1, timeouts)
-	quicksort(arr, j + 1, end, timeouts)
+  if (start >= end) {
+    return
+  }
+  let i = start,
+    j = end
+  let pivot = arr[start]
+  while (i < j) {
+    while (pivot >= arr[i] && i < j) {
+      setSelectFrontBar(i, timeouts)
+      i++
+    }
+    while (pivot < arr[j] && i <= j) {
+      setSelectBackBar(j, arr.length - 1, timeouts)
+      j--
+    }
+    if (i < j) {
+      swap(arr, i, j, timeouts)
+      j--
+    }
+  }
+  swap(arr, start, j, timeouts)
+  quicksort(arr, start, j - 1, timeouts)
+  quicksort(arr, j + 1, end, timeouts)
 }
 
-const startQuickSort = (arr, start, end, timeouts) => {
-	delay = 0
-	quicksort(arr, start, end, timeouts)
-	for (let i = 0; i < arr.length; i++) {
-		timeouts.push(
-			setTimeout(() => {
-				document.getElementById(`sort-${i}`).classList.remove('bar-select')
-			}, delay * 2 + 10 * i),
-		)
-	}
-	return timeouts
+const startQuickSort = (arr, start, end, timeouts, cb) => {
+  delay = 0
+  quicksort(arr, start, end, timeouts)
+  for (let i = 0; i < arr.length; i++) {
+    timeouts.push(
+      setTimeout(() => {
+        document.getElementById(`sort-${i}`).classList.remove('bar-select')
+      }, delay * time + 10 * i),
+    )
+  }
+  timeouts.push(
+    setTimeout(() => {
+      cb(arr)
+    }, delay * time + 10 * arr.length),
+  )
+  return timeouts
 }
 
 export default startQuickSort
--- src/containers/SortVisualizer/SortVisualizer.js
+++ src/containers/SortVisualizer/SortVisualizer.js
@@ -5,88 +5,90 @@ import QuickSort from '/algorithms/quick_sort'
 import './SortVisualizer.scss'
 
 class SortVisualizer extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			array: [],
-			timeouts: [],
-		}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      array: [],
+      timeouts: [],
+    }
+  }
 
-	componentDidMount() {
-		this.setArray()
-	}
+  componentDidMount() {
+    this.setArray()
+  }
 
-	randomIntFromInterval(min, max) {
-		return Math.floor(Math.random() * (max - min + 1) + min)
-	}
+  randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
 
-	setArray() {
-		const array = []
-		for (let i = 0; i < 100; i++) {
-			array.push(this.randomIntFromInterval(5, 400))
-		}
-		this.setState({ array, timeouts: [] })
-	}
+  setArray() {
+    const array = []
+    for (let i = 0; i < 100; i++) {
+      array.push(this.randomIntFromInterval(5, 400))
+    }
+    this.setState({ array, timeouts: [] })
+  }
 
-	resetArray() {
-		const { timeouts } = this.state
-		for (let i = 0; i < timeouts.length; i++) {
-			clearTimeout(timeouts[i])
-		}
-		for (let i = 0; i < 100; i++) {
-			document.getElementById(`sort-${i}`).classList.remove('bar-select')
-		}
-	}
+  resetArray() {
+    const { timeouts } = this.state
+    for (let i = 0; i < timeouts.length; i++) {
+      clearTimeout(timeouts[i])
+    }
+    for (let i = 0; i < 100; i++) {
+      document.getElementById(`sort-${i}`).classList.remove('bar-select')
+    }
+  }
 
-	quickSort() {
-		this.resetArray()
-		const { array, timeouts } = this.state
-		const newArray = array.map(data => data)
-		const newTimeouts = QuickSort(newArray, 0, newArray.length - 1, timeouts)
-		this.setState({ timeouts: newTimeouts })
-	}
+  quickSort() {
+    this.resetArray()
+    const { array, timeouts } = this.state
+    const newArray = array.map(data => data)
+    const newTimeouts = QuickSort(newArray, 0, newArray.length - 1, timeouts, (arr) => this.setState({ array: arr }, () => {
+      console.log(this.state.array)
+    }))
+    this.setState({ timeouts: newTimeouts })
+  }
 
-	bubbleSort() {
-		this.resetArray()
-		const { array, timeouts } = this.state
-		const newTimeouts = BubbleSort(array, timeouts)
-		this.setState({ timeouts: newTimeouts })
-	}
+  bubbleSort() {
+    this.resetArray()
+    const { array, timeouts } = this.state
+    const newTimeouts = BubbleSort(array, timeouts)
+    this.setState({ timeouts: newTimeouts })
+  }
 
-	render() {
-		const { array } = this.state
-		return (
-			<Fragment>
-				<div className='bar-container'>
-					{array.map((value, idx) => {
-						return (
-							<div
-								className='bar'
-								style={{ height: `${value}px` }}
-								key={`sort-${idx}`}
-								id={`sort-${idx}`}
-							/>
-						)
-					})}
-				</div>
-				<Button
-					class=''
-					clicked={() => {
-						this.resetArray()
-						this.setArray()
-					}}>
-					New Pattern
+  render() {
+    const { array } = this.state
+    return (
+      <Fragment>
+        <div className='bar-container'>
+          {array.map((value, idx) => {
+            return (
+              <div
+                className='bar'
+                style={{ height: `${value}px` }}
+                key={`sort-${idx}`}
+                id={`sort-${idx}`}
+              />
+            )
+          })}
+        </div>
+        <Button
+          class=''
+          clicked={() => {
+            this.resetArray()
+            this.setArray()
+          }}>
+          New Pattern
 				</Button>
-				<Button class='' clicked={() => this.bubbleSort()}>
-					Bubble Sort
+        <Button class='' clicked={() => this.bubbleSort()}>
+          Bubble Sort
 				</Button>
-				<Button class='' clicked={() => this.quickSort()}>
-					Quick Sort
+        <Button class='' clicked={() => this.quickSort()}>
+          Quick Sort
 				</Button>
-			</Fragment>
-		)
-	}
+      </Fragment>
+    )
+  }
 }
 
 export default SortVisualizer
